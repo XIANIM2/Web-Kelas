a02c2aefb84ec6327bb5f417fb1ae302b4550170
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-11-22 22:53:35 +0700

    add murid pages

--- src/pages/murid.js
+++ src/pages/murid.js
@@ -1,9 +1,26 @@
 import NavBar from "../components/navBar";
+import murid from "../data/murid.json";
 
 export default function Murid() {
+    const Data = [];
+    murid.forEach(x => Data.push(
+        <div className="bg-card rounded-md p-3 flex space-x-4">
+            <img src={`../data/images/contoh.png`} alt="foto" width="150" height="150" />
+            <div className="font-mono">
+                <p>{`Nama: ${x.nama}`}</p>
+                <p>{`Absen: ${x.absen}`}</p>
+                <p>{`Jenis Kelamin: ${x.gender}`}</p>
+                <p>{`Tanggal Lahir: ${x.birth}`}</p>
+            </div>
+        </div>
+    ))
+
     return (
         <>
             <NavBar />
+            <div className="p-10 grid space-y-4">
+                {Data}
+            </div>
         </>
     )
 }
