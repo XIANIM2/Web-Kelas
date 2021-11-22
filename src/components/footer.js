bc1108843be11a68cc8ab759ea9fc0f781b9b444
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-11-22 22:29:28 +0700

    change footer color to gray

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
         <div className="bg-black h-auto">
-            <div className="flex justify-center text-white text-center text-xs space-x-3">
+            <div className="flex justify-center text-gray-400 text-center text-xs space-x-3">
                 <p>{'Coded by Vann-Dev (Stevan)'}</p>
-                <p className="font-bold text-gray-400">-</p>
+                <p className="font-bold">-</p>
                 <p>Designed by Erik</p>
             </div>
         </div>
