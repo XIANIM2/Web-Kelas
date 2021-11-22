9e893b4161aae4e6710a3c1e6449b6c66f9250cd
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-11-22 22:25:47 +0700

    change footer color

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <div className="bg-black h-auto">
             <div className="flex justify-center text-white text-center text-xs space-x-3">
                 <p>{'Coded by Vann-Dev (Stevan)'}</p>
-                <p className="font-bold">-</p>
+                <p className="font-bold text-gray-400">-</p>
                 <p>Designed by Erik</p>
             </div>
         </div>
