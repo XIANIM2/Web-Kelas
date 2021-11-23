21793d5f966da98cf59a86006d9ed1027f85d977
Vann <82990248+Stevanvincent@users.noreply.github.com>
2021-11-23 19:49:49 +0700

    feat(murid): fix some breaking change

--- src/pages/murid.js
+++ src/pages/murid.js
@@ -1,16 +1,18 @@
 import NavBar from "../components/navBar";
-import murid from "../data/murid.json";
+import murid from "./murid.json";
 
 export default function Murid() {
     const Data = [];
     murid.forEach(x => Data.push(
-        <div className="bg-card rounded-md p-3 flex space-x-4">
-            <img src={`../data/images/contoh.png`} alt="foto" width="150" height="150" />
-            <div className="font-mono">
-                <p>{`Nama: ${x.nama}`}</p>
-                <p>{`Absen: ${x.absen}`}</p>
-                <p>{`Jenis Kelamin: ${x.gender}`}</p>
-                <p>{`Tanggal Lahir: ${x.birth}`}</p>
+        <div className="bg-card rounded-md">
+            <div className="mt-auto mb-auto space-x-4 flex p-5">
+                <img src={`/data/images/${x.foto}`} alt="profil" height="20px" width="auto"></img>
+                <div className="font-mono text-xs">
+                    <p key={x.nama}><span key={x.nama} className="font-semibold">Nama:</span>{`${x.nama}`}</p>
+                    <p key={x.absen}><span key={x.absen} className="font-semibold">Absen:</span>{`${x.absen}`}</p>
+                    <p key={x.gender}><span key={x.gender} className="font-semibold">Jenis Kelamin:</span>{`${x.gender}`}</p>
+                    <p key={x.birth.toString()}><span key={x.birth.toString()} className="font-semibold">Tanggal Lahir:</span>{`${x.birth}`}</p>
+                </div>
             </div>
         </div>
     ))
