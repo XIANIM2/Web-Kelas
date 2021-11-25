0e866a25ffacf33cb94b49a4021c03d14a75da96
Rikku <riecmilo@gmail.com>
2021-11-25 09:02:12 +0700

    Update Data

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="bg-black h-auto w-full fixed bottom-0">
             <div className="flex justify-center text-gray-400 text-center text-xs space-x-3">
-                <p>{'Coded by Vann-Dev (Stevan)'}</p>
+                <p>Coded by Stevan</p>
                 <p className="font-bold">-</p>
                 <p>Designed by Erik</p>
             </div>
